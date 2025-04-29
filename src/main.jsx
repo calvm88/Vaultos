9706fffbb1059ae6f4ccaf096fa086fa3a39856e
calvm88 <calvinmadden08@gmail.com>
2025-04-29 17:46:18 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App.jsx';
+import App from "./App.jsx";
 import '../styles/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -8,3 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+fix import path from app
