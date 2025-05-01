2b4994c8f15360d9263727f5bf86b31785336590
calvm88 <calvinmadden08@gmail.com>
2025-05-01 17:51:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from 'react';
 import Sidebar from './Sidebar';
 import CentralPanel from './CentralPanel';
 import VaultLandingPage from './VaultLandingPage';
+import { AnimatePresence } from 'framer-motion';
 
 export default function App() {
   const [enteredVault, setEnteredVault] = useState(false);
 
   return (
     <div className="min-h-screen bg-black text-green-400 font-mono">
-      {enteredVault ? (
-        <div className="flex h-screen">
-          <Sidebar setActiveTab={() => {}} onXP={() => {}} />
-          <CentralPanel />
-        </div>
-      ) : (
-        <VaultLandingPage onEnterVault={() => setEnteredVault(true)} />
-      )}
+      <AnimatePresence mode="wait">
+        {enteredVault ? (
+          <div key="vault" className="flex h-screen">
+            <Sidebar setActiveTab={() => {}} onXP={() => {}} />
+            <CentralPanel />
+          </div>
+        ) : (
+          <VaultLandingPage key="landing" onEnterVault={() => setEnteredVault(true)} />
+        )}
+      </AnimatePresence>
     </div>
   );
 }
